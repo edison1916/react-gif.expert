496483c8dffa69df52fd78e92f2754688d0b99ec
EDISON ARGIRO ACEVEDO PINEDA <EACEVEDO@EXPERTOS.COM>
2022-09-29 10:58:08 -0500

    primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { AddCategory, GifGrid} from "./components";
+
+
+
+export const GitExpertApp = () => {
+
+    const [categories, setCategories] = useState([' One Punch', 'Dragon Ball']);
+
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
+
+        
+        setCategories([newCategory, ...categories]);
+       
+
+    }
+
+    return (
+        <>
+            <h1>GitExpertApp</h1>
+
+            <AddCategory
+
+                onNewCategory={(value) => onAddCategory(value)}
+            />
+                {
+                    categories.map((category) => (
+                        <GifGrid key={category}
+                            category={category}
+                        />
+                    ))
+                }
+        </>
+    )
+}
